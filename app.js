ec4566ecba1ebebe65c3da7d34d870c65e8dd3af
Daily <saschanoah22@gmail.com>
2026-01-03 22:11:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -399,6 +399,8 @@ setInterval(collectNews, REFRESH_INTERVAL);
 // ================= INTRO JS =================
 document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.getElementById("introOverlay");
+  // setTimeout(() => card.classList.add("show"), 200);
+
   const card = document.getElementById("introCard");
   const textEl = document.getElementById("introText");
   const button = document.getElementById("introButton");
